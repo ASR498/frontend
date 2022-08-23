b7d7cd139dc4e1d622d0f7bbee2c9e4d985d51ff
ASR498 <adityarana498@gmail.com>
2022-08-24 09:52:55 +1200

    new version changes impt

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import {
   Route,
+  Routes,
   NavLink,
   BrowserRouter as Router
 } from "react-router-dom";
@@ -14,19 +15,25 @@ const App = () =>  {
     return (
         <Router>
         <div>
+        
           <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
             <ul className="header">
 
-                <li><NavLink exact to = "/">Home</NavLink></li>
+
+                
+                <li><NavLink to = "/">Home</NavLink></li>
                 <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
                 <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
+                
 
             </ul>
           <div className="content">
+            <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route  path="/SEPractice" element={<SEPractice />}/>
+            <Route  path="/SubmitArticle" element={<SubmitArticle/>}/>
+            </Routes>
 
-            <Route exact path="/" component={Home}/>
-            <Route  path="/SEPractice" component={SEPractice}/>
-            <Route  path="/SubmitArticle" component={SubmitArticle}/>
             
           </div>
         </div>
